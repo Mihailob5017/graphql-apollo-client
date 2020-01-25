1e69d7280888c58b9de34d15420488253e38b505
Mihailo Boskovic <mihailob5017@gmail.com>
2020-01-26 00:05:21 +0100

    inital commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const expressGraphQL=require('express-graphql');
+const app=express();
+const schema=require('./schema');
+
+app.use("/graphql",expressGraphQL({
+    schema,
+    graphiql:true,
+}))
+
+
+app.listen('5000',()=>{
+    console.log('Server Started');
+})
